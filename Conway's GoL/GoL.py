e16430104f0a63d1a9d25ff31a25a64abc62852d
juliahill23 <julia.hill23@gmail.com>
2021-05-04 16:20:40 -0700

    Set up randomized grid, wrote function templates for getNextGrid, getLiveNeighbours.

--- Conway's GoL/GoL.py
+++ Conway's GoL/GoL.py
@@ -1,1 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+gridSize = input("What size of grid would you like? ")
+
+grid = np.random.randint(0, 2, size=(gridSize, gridSize))
+
+def getLiveNeighbours():
+
+# funcAnimation function, called at each frame
+# first argument will be the next value in frames
+def getNextGrid(frameNum, *fargs):
