4ca778f8c3e1b8f0d9aecaa7f176c42b0dc7a2f4
juliahill23 <julia.hill23@gmail.com>
2021-05-28 15:02:05 -0700

    Implementing class subobject, superobject
    Started a stub for the class superobject, with get and set methods for each feature. Wrote the Bard subclass file. Might alter how choosing tools and skills is handled in future.

---
+++ DnD Interface/Classes/Bard.py
@@ -0,0 +1,22 @@
+class Bard(ClassParent):
+    def __init__(self):
+        ClassParent.__init__(self)
+
+        self.hitdie = '1d8'
+        self.saves[1] = 1 # DEX
+        self.saves[6] = 1 # CHA
+
+        self.toolprofs = []
+        self.toolchoose = [3, ['Bagpipes', 'Drum', 'Dulcimer', 'Flute', 'Horn', \
+                            'Lute', 'Lyre', 'Pan Flute', 'Shawm', 'Viol']]
+
+        self.skillsinfo = []
+        self.skillschoose = [3, ['Acrobatics', 'Animal Handling', 'Arcana', \
+                            'Athletics', 'Deception', 'History', 'Insight', \
+                            'Intimidation', 'Investigation', 'Medicine', \
+                            'Nature', 'Perception', 'Performance', \
+                            'Persuasion', 'Religion', 'Sleight of Hand', \
+                            'Stealth', 'Survival']]
+
+        self.otherprofs = ['Light', 'Simple', 'Hand Crossbow', 'Longsword', \
+                            'Rapier', 'Shortsword']
---
+++ DnD Interface/Classes/Class_Parent.py
@@ -0,0 +1,62 @@
+class ClassParent:
+
+    saves = [0, 0, 0, 0, 0, 0, 0]
+    hitdie = ''
+    toolprofs = []      # tool proficiencies
+    toolchoose = [0]    # 0th el is number of tools one can choose from,
+                        # 1st el is list of the choices
+    skillsinfo = []     # skill proficiencies
+    skillschoose = [0]  # 0th el is number of skills one can choose from,
+                        # 1st el is list of the choices
+    otherprofs = []     # weapon and armor proficiencies
+
+    def __init__(self):
+        self.saves = saves
+        self.hitdie = hitdie
+        self.toolprofs = toolprofs
+        self.toolchoose = toolchoose
+        self.skillsinfo = skillsinfo
+        self.skillschoose = skillschoose
+        self.otherprofs = otherprofs
+
+    def getSaves(self):
+        return self.saves
+
+    def getHitDie(self):
+        return self.hitdie
+
+    def getToolProfs(self):
+        return self.toolprofs
+
+    def getToolChoose(self):
+        return self.toolchoose
+
+    def getSkillInfo(self):
+        return self.skillinfo
+
+    def getSkillsChoose(self):
+        return self.skillschoose
+
+    def getOtherProfs(self):
+        return self.otherprofs
+
+    def setSaves(self, newsaves):
+        self.saves = newsaves
+
+    def setHitDie(self, newhitdie):
+        self.hitdie = newhitdie
+
+    def setToolProfs(self, newtools):
+        self.toolprofs = newtools
+
+    def setToolChoose(self, newtoolschoose):
+        self.toolchoose = newtoolschoose
+
+    def setSkillInfo(self, newskillinfo):
+        self.skillinfo = newskillinfo
+
+    def setSkillsChoose(self, newskillschoose):
+        self.skillschoose = newskillschoose
+
+    def setOtherProfs(self, newother):
+        self.otherprofs = newother
