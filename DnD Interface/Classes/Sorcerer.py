ec6dcb6eb764e5d9dd409c0b6702b8351c624076
scibere <samanthacibere@gmail.com>
2021-06-16 20:42:44 -0700

    from the bottom, finished rogue

--- DnD Interface/Classes/Sorcerer.py
+++ DnD Interface/Classes/Sorcerer.py
@@ -1,15 +1,48 @@
+import numpy as np
+
 class Sorcerer(ClassParent):
-    def __init__(self):
-        ClassParent.__init__(self)
+    def __init__(self, mc, stats):
+        ClassParent.__init__(self, mc)
 
         self.hitdie = '1d6'
-        self.saves[2] = 1 # CON
-        self.saves[5] = 1 # CHA
+        self.multiclassing = [0, 0, 0, 0, 0, 1]
+        self.subclass = ''
+        self.abilities = {1: {'Spellcasting': "An event in your past, or in the life of a parent or ancestor, left an indelible mark on you, infusing you with arcane magic. This font of magic, whatever its origin, fuels your spells. See chapter 10 for the general rules of spellcasting and chapter 11 for the sorcerer spell list.",
+                              'Cantrips': "At 1st level, you know four cantrips of your choice from the sorcerer spell list. You learn an additional sorcerer cantrip of your choice at 4th level and another at 10th level.",
+                              'Spell Slots': "The Sorcerer table shows how many spell slots you have to cast your sorcerer spells of 1st level and higher. To cast one of these sorcerer spells, you must expend a slot of the spell's level or higher. You regain all expended spell slots when you finish a long rest.\nFor example, if you know the 1st-level spell burning hands and have a 1st-level and a 2nd-level spell slot available, you can cast burning hands using either slot.",
+                              'Spellcasting Ability': "Charisma is your spellcasting ability for your sorcerer spells, since the power of your magic relies on your ability to project your will into the world. You use your Charisma whenever a spell refers to your spellcasting ability. In addition, you use your Charisma modifier when setting the saving throw DC for a sorcerer spell you cast and when making an attack roll with one.\nSpell save DC = 8 + your proficiency bonus + your Charisma modifier\nSpell attack modifier = your proficiency bonus + your Charisma modifier",
+                              'Spellcasting Focus': "You can use an arcane focus as a spellcasting focus for your sorcerer spells.",
+                              'Spells Known of 1st Level and Higher': "You know two 1st-level spells of your choice from the sorcerer spell list.\nYou learn an additional sorcerer spell of your choice at each level except 12th, 14th, 16th, 18th, 19th, and 20th. Each of these spells must be of a level for which you have spell slots. For instance, when you reach 3rd level in this class, you can learn one new spell of 1st or 2nd level.\nAdditionally, when you gain a level in this class, you can choose one of the sorcerer spells you know and replace it with another spell from the sorcerer spell list, which also must be of a level for which you have spell slots.",
+                              'Sorcerous Origin': "Choose a sorcerous origin, which describes the source of your innate magical power, from the list of available origins.\nYour choice grants you features when you choose it at 1st level and again at 6th, 14th, and 18th level."},
+                          2: {'Font of Magic': "At 2nd level, you tap into a deep wellspring of magic within yourself. This wellspring is represented by sorcery points, which allow you to create a variety of magical effects.",
+                              'Sorcery Points': "You have 2 sorcery points, and you gain one additional point every time you level up, to a maximum of 20 at level 20. You can never have more sorcery points than shown on the table for your level. You regain all spent sorcery points when you finish a long rest.",
+                              'Flexible Casting': "You can use your sorcery points to gain additional spell slots, or sacrifice spell slots to gain additional sorcery points. You learn other ways to use your sorcery points as you reach higher levels.\nCreating Spell Slots. You can transform unexpended sorcery points into one spell slot as a bonus action on your turn. The created spell slots vanish at the end of a long rest. The Creating Spell Slots table shows the cost of creating a spell slot of a given level. You can create spell slots no higher in level than 5th.\nCreating Spell Slots\nSpell Slot Level	Sorcery Point Cost\n1st	2\n2nd	3\n3rd	5\n4th	6\n5th	7\nConverting a Spell Slot to Sorcery Points. As a bonus action on your turn, you can expend one spell slot and gain a number of sorcery points equal to the slot's level."},
+                          3: {'Metamagic': "At 3rd level, you gain the ability to twist your spells to suit your needs. You gain two of the following Metamagic options of your choice. You gain another one at 10th and 17th level.\nYou can use only one Metamagic option on a spell when you cast it, unless otherwise noted."},
+                          4: {'Ability Score Improvement': "When you reach 4th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          8: {'Ability Score Improvement': "When you reach 8th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          10: {'Metamagic': "At 10th level, you learn an additional metamagic option."},
+                          12: {'Ability Score Improvement': "When you reach 12th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          16: {'Ability Score Improvement': "When you reach 16th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          17: {'Metamagic': "At 17th level, you learn an additional metamagic option."},
+                          19: {'Ability Score Improvement': "When you reach 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          20: {'Sorcerous Restoration': "At 20th level, you regain 4 expended sorcery points whenever you finish a short rest"}}
 
-        self.toolprofs = []
+        if not mc:
+            self.saves[2] = 1 # CON
+            self.saves[5] = 1 # CHA
 
-        self.skillsinfo = []
-        self.skillschoose = [3, ['Arcana', 'Deception', 'Insight',
+            self.toolprofs = []
+
+            self.skillsinfo = []
+            self.skillschoose = [3, ['Arcana', 'Deception', 'Insight',
                                  'Intimidation', 'Persuasion', 'Religion']]
 
-        self.otherprofs = ['Dagger', 'Light Crossbow', 'Dart', 'Sling', 'Quarterstaff']
+            self.otherprofs = ['Dagger', 'Light Crossbow', 'Dart', 'Sling', 'Quarterstaff']
+            return True
+
+        elif mc and np.and(stats > (multiclassing * 13)):
+
+            return True
+
+        else:
+            return False
--- DnD Interface/Classes/Warlock.py
+++ DnD Interface/Classes/Warlock.py
@@ -1,15 +1,48 @@
+import numpy as np
+
 class Warlock(ClassParent):
-    def __init__(self):
-        ClassParent.__init__(self)
+    def __init__(self, mc, stats):
+        ClassParent.__init__(self, mc)
 
         self.hitdie = '1d8'
-        self.saves[4] = 1 # WIS
-        self.saves[5] = 1 # CHA
+        self.multiclassing = [0, 0, 0, 0, 0, 1]
+        self.subclass = ''
+        self.abilities = {1: {'Pact Magic': "Your arcane research and the magic bestowed on you by your patron have given you facility with spells. See chapter 10 for the general rules of spellcasting and chapter 11 for the warlock spell list.",
+                              'Cantrips': "You know two cantrips of your choice from the warlock spell list. You learn additional warlock cantrips of your choice at higher levels, as shown in the Cantrips Known column of the Warlock table.",
+                              'Spell Slots': "The Warlock table shows how many spell slots you have to cast your warlock spells of 1st through 5th level. The table also shows what the level of those slots is; all of your spell slots are the same level. To cast one of your warlock spells of 1st level or higher, you must expend a spell slot. You regain all expended spell slots when you finish a short or long rest.\nFor example, when you are 5th level, you have two 3rd-level spell slots. To cast the 1st-level spell witch bolt, you must spend one of those slots, and you cast it as a 3rd-level spell.",
+                              'Spellcasting Ability': "Charisma is your spellcasting ability for your warlock spells, so you use your Charisma whenever a spell refers to your spellcasting ability. In addition, you use your Charisma modifier when setting the saving throw DC for a warlock spell you cast and when making an attack roll with one.\nSpell save DC = 8 + your proficiency bonus + your Charisma modifier\nSpell attack modifier = your proficiency bonus + your Charisma modifier", 'Spellcasting Focus': "You can use an arcane focus as a spellcasting focus for your warlock spells.",
+                              'Spells Known of 1st Level and Higher': "At 1st level, you know two 1st-level spells of your choice from the warlock spell list.\nThe Spells Known column of the Warlock table shows when you learn more warlock spells of your choice of 1st level and higher. A spell you choose must be of a level no higher than what's shown in the table's Slot Level column for your level. When you reach 6th level, for example, you learn a new warlock spell, which can be 1st, 2nd, or 3rd level.\nAdditionally, when you gain a level in this class, you can choose one of the warlock spells you know and replace it with another spell from the warlock spell list, which also must be of a level for which you have spell slots.",
+                              'Otherwordly Patron': "At 1st level, you have struck a bargain with an otherworldly being chosen from the list of available patrons. Your choice grants you features at 1st level and again at 6th, 10th, and 14th level."},
+                          2: {'Eldritch Invocations': "In your study of occult lore, you have unearthed eldritch invocations, fragments of forbidden knowledge that imbue you with an abiding magical ability.\nAt 2nd level, you gain two eldritch invocations of your choice. A list of the available options can be found on the Optional Features page. When you gain certain warlock levels, you gain additional invocations of your choice, as shown in the Invocations Known column of the Warlock         table.\nAdditionally, when you gain a level in this class, you can choose one of the invocations you know and replace it with another invocation that you could learn at that level.\nIf an eldritch invocation has prerequisites, you must meet them to learn it. You can learn the invocation at the same time that you meet its prerequisites. A level prerequisite refers to your level in this class."},
+                          3: {'Pact Boon': "At 3rd level, your otherworldly patron bestows a gift upon you for your loyal service. "},
+                          4: {'Ability Score Improvement': "When you reach 4th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          8: {'Ability Score Improvement': "When you reach 8th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          11: {'Mystic Arcanum': "At 11th level, your patron bestows upon you a magical secret called an arcanum. Choose one 6th-level spell from the warlock spell list as this arcanum.\nYou can cast your arcanum spell once without expending a spell slot. You must finish a long rest before you can do so again.\nAt higher levels, you gain more warlock spells of your choice that can be cast in this way: one 7th-level spell at 13th level, one 8th-level spell at 15th level, and one 9th-level spell at 17th level. You regain all uses of your Mystic Arcanum when you finish a long rest."},
+                          12: {'Ability Score Improvement': "When you reach 12th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          13: {'Mystic Arcanum': "At 13th level, your patron bestows upon you a magical secret called an arcanum. Choose one 7th-level spell from the warlock spell list as this arcanum.\nYou can cast your arcanum spell once without expending a spell slot. You must finish a long rest before you can do so again."},
+                          15: {'Mystic Arcanum': "At 15th level, your patron bestows upon you a magical secret called an arcanum. Choose one 8th-level spell from the warlock spell list as this arcanum.\nYou can cast your arcanum spell once without expending a spell slot. You must finish a long rest before you can do so again."},
+                          16: {'Ability Score Improvement': "When you reach 16th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          17: {'Mystic Arcanum': "At 17th level, your patron bestows upon you a magical secret called an arcanum. Choose one 9th-level spell from the warlock spell list as this arcanum.\nYou can cast your arcanum spell once without expending a spell slot. You must finish a long rest before you can do so again."},
+                          19: {'Ability Score Improvement': "When you reach 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          20: {'Eldritch Master': "At 20th level, you can draw on your inner reserve of mystical power while entreating your patron to regain expended spell slots. You can spend 1 minute entreating your patron for aid to regain all your expended spell slots from your Pact Magic feature. Once you regain spell slots with this feature, you must finish a long rest before you can do so again."}}
 
-        self.toolprofs = []
+        if not mc:
+            self.saves[4] = 1 # WIS
+            self.saves[5] = 1 # CHA
 
-        self.skillsinfo = []
-        self.skillschoose = [2, ['Arcana', 'Deception', 'History',
+            self.toolprofs = []
+
+            self.skillsinfo = []
+            self.skillschoose = [2, ['Arcana', 'Deception', 'History',
                                  'Intimidation', 'Investigation', 'Nature', 'Religion']]
 
-        self.otherprofs = ['Light', 'Simple']
+            self.otherprofs = ['Light', 'Simple']
+
+            return True
+        elif mc and np.and(stats > (multiclassing * 13)):
+            self.otherprofs = ['Light', 'Simple']
+
+            return True
+
+        else:
+            return False
