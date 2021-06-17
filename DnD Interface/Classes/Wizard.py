c4eba72676fe453d5fdfc6f6f06bc8de0ca3f628
scibere <samanthacibere@gmail.com>
2021-06-16 17:30:50 -0700

    Update Wizard.py

--- DnD Interface/Classes/Wizard.py
+++ DnD Interface/Classes/Wizard.py
@@ -1,15 +1,37 @@
-class Warlock(ClassParent):
-    def __init__(self):
-        ClassParent.__init__(self)
+import numpy as np
+
+class Wizard(ClassParent):
+    def __init__(self, mc, stats):
+        ClassParent.__init__(self, mc)
 
         self.hitdie = '1d6'
-        self.saves[4] = 1 # WIS
-        self.saves[3] = 1 # INT
+        self.multiclassing = [0, 0, 0, 0, 1, 0]
+        self.subclass = ''
+        self.abilities = {1: {'Arcane Recovery': "You have learned to regain some of your magical energy by studying your spellbook. Once per day when you finish a short rest, you can choose expended spell slots to recover. The spell slots can have a combined level that is equal to or less than half your wizard level (rounded up), and none of the slots can be 6th level or higher.\nFor example, if you're a 4th-level wizard, you can recover up to two levels worth of spell slots.\nYou can recover  either a 2nd-level spell slot or two 1st-level spell slots.", 'Spellcasting': "As a student of arcane magic, you have a spellbook containing spells that show the first glimmerings of your true power.", 'Cantrips': "At 1st level, you know three cantrips of your choice from the wizard spell list. You learn additional wizard cantrips of your choice at higher levels, as shown in the Cantrips Known column of the Wizard table.", 'Spellbook': "At 1st level, you have a spellbook containing six 1st-level wizard spells of your choice. Your spellbook is the repository of the wizard spells you know, except your cantrips, which are fixed in your mind.", 'Preparing and Casting Spells': "The Wizard table shows how many spell slots you have to cast your spells of 1st level and higher. To cast one of these spells, you must expend a slot of the spell's level or higher. You regain all expended spell slots when you finish a long rest.\nYou prepare the list of wizard spells that are\navailable for you to cast. To do so, choose a number of wizard spells from your spellbook equal to your Intelligence modifier + your wizard level (minimum of one spell). The spells must be of a level for which you have spell slots.\nFor example, if you're a 3rd-level wizard, you have four 1st-level and two 2nd-level spell slots. With an Intelligence of 16, your list of prepared spells can include six spells of 1st or 2nd level, in any combination, chosen from your spellbook. If you prepare the 1st-level spell magic missile, you can cast it using a 1st-level or a 2nd-level slot. Casting the spell doesn't remove it from your list of prepared spells.\nYou can change your list of prepared spells when you finish a long rest. Preparing a new list of wizard spells requires time spent studying your spellbook and memorizing the incantations and gestures you must make to cast the spell: at least 1 minute per spell level for each spell on your list.", 'Spellcasting Ability': "Intelligence is your spellcasting ability for your wizard spells, since you learn your wizard spells through dedicated study and memorization. You use your Intelligence whenever a spell refers to your spellcasting ability. In addition, you use your Intelligence modifier when setting the saving throw DC for a wizard spell you cast and when making an attack roll with one.\nSpell save DC = 8 + your proficiency bonus + your Intelligence modifier\nSpell attack modifier = your proficiency bonus + your Intelligence modifier", 'Ritual Casting': "You can cast a wizard spell as a ritual if that spell has the ritual tag and you have the spell in your spellbook. You don't need to have the spell prepared.", 'Spellcasting Focus': "You can use an arcane focus as a spellcasting focus for your wizard spells.", 'Learning Spells of 1st Level and Higher': "Each time you gain a wizard level, you can add two wizard spells of your choice to your spellbook. Each of these spells must be of a level for which you have spell slots, as shown on the Wizard table. On your adventures, you might find other spells that you can add to your spellbook (see NEEDS REFRACTORING)."},
+                          2: {'Arcane Tradition': "When you reach 2nd level, you choose an arcane tradition from the list of available traditions, shaping your practice of magic. Your choice grants you features at 2nd level and again at 6th, 10th, and 14th level."},
+                          4: {'Ability Score Improvement': "When you reach 4th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          8: {'Ability Score Improvement': "When you reach 8th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          12: {'Ability Score Improvement': "When you reach 12th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          16: {'Ability Score Improvement': "When you reach 16th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          18: {'Spell Mastery': "At 18th level, you have achieved such mastery over certain spells that you can cast them at will. Choose a 1st-level wizard spell and a 2nd-level wizard spell that are in your spellbook. You can cast those spells at their lowest level without expending a spell slot when you have them prepared. If you want to cast either spell at a higher level, you must expend a spell slot as normal.\nBy spending 8 hours in study, you can exchange one or both of the spells you chose for different spells of the same levels."},
+                          19: {'Ability Score Improvement': "When you reach 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can't increase an ability score above 20 using this feature.\nIf your DM allows the use of feats, you may instead take a feat."},
+                          20: {'Signature Spells': "When you reach 20th level, you gain mastery over two powerful spells and can cast them with little effort. Choose two 3rd-level wizard spells in your spellbook as your signature spells. You always have these spells prepared, they don't count against the number of spells you have prepared, and you can cast each of them once at 3rd level without expending a spell slot. When you do so, you can't do so again until you finish a short or long rest.\nIf you want to cast either spell at a higher level, you must expend a spell slot as normal."}}
 
-        self.toolprofs = []
+        if not mc:
+            self.saves[4] = 1 # WIS
+            self.saves[3] = 1 # INT
 
-        self.skillsinfo = []
-        self.skillschoose = [2, ['Arcana', 'Insight', 'History',
-                                 'Medicine', 'Investigation', 'Religion']]
+            self.toolprofs = []
 
-        self.otherprofs = ['Light Crossbow', 'Dagger', 'Dart', 'Sling', 'Quarterstaff']
+            self.skillsinfo = []
+            self.skillschoose = [2, ['Arcana', 'Insight', 'History',
+                                     'Medicine', 'Investigation', 'Religion']]
+
+            self.otherprofs = ['Light Crossbow', 'Dagger', 'Dart', 'Sling', 'Quarterstaff']
+
+            return True
+        elif mc and np.and(stats > (multiclassing * 13)):
+            return True
+
+        else:
+            return False
